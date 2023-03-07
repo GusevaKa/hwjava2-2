1265b2cc748aa51dc3cc4998699ed56afae9400c
Guseva <tyurinaekaterina@yandex.ru>
2023-03-07 14:13:42 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int account = 100;
+        int refill = 900;
+        int result = (account + refill);
+        int bonus = (refill / 100);
+
+        if (refill >= 1000) {
+            System.out.println (" Итоговый счет " + (bonus + result));
+            System.out.println("Бонусные рубли" + bonus);
+        } else {
+            System.out.println(" Итоговый счет" + result);
+        }
+
+        }
+    }
